4264105b21a393bcb85750f745db2aeb7acbad98
tomasmaggio <tomasmaggio220@gmail.com>
2022-03-22 17:39:03 -0300

    subo ej 1

---
+++ FUNDAMENTOS/min.js
@@ -0,0 +1,17 @@
+var number = 6;
+
+var x = 44
+if(x==44){
+    var x = 74
+    console.log(x)
+
+}
+console.log(x)
+
+
+let y = 11;
+if(y == 11){
+    let = 22;
+    console.log(y)
+}
+console.log(y)
