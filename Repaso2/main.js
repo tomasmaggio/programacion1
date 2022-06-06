e0cabf44aeada9fa9b101d23e4c0ae2508877141
tomasmaggio <tomasmaggio220@gmail.com>
2022-06-06 15:30:19 -0300

    as

---
+++ Repaso2/main.js
@@ -0,0 +1,51 @@
+let consolas= ['PlayStation', 'Xbox', 'Nintendo Game Cube', 'Sepa Dreamcast','Gameboy Advanced', 'Pokemon Mini']
+
+//punto 2
+consolas.push("Nintendo 64")
+console.log(consolas)
+
+//punto 3
+consolas.splice(4,2)
+console.log(consolas)
+
+//punto 4
+function mostrar (){
+    const longitud_array = consolas.length
+    const ultimo_el = consolas[consolas.length-1]
+    const elemento_4 = consolas[4]
+
+    document.getElementById("mostrar_p").textContent=`
+
+            La longitud de el arreglo es: ${longitud_array},
+            El ultimo elemento del arreglo es: ${ultimo_el},
+            Y el cuarto elemento del arreglo es: ${elemento_4}
+    
+    
+    `
+}
+const boton = document.getElementById("btn_calcular")
+boton.addEventListener("click", mostrar)
+
+
+
+
+
+let con = []
+const recorrido = () => {
+
+    consolas.forEach((index,element) => {
+        
+        let fila = `
+                    <tr>
+                    <th>${element}</th>
+                    <td>${index}</td>
+                    </tr>
+        `
+
+        con.push(fila)
+    });
+
+    document.getElementById('tbody').innerHTML=con.join('')
+}
+
+document.getElementById('btn_tabla').addEventListener("click",recorrido)
